76961385c1b43b21d1db61b24367e8033d9f62a2
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-17 11:33:43 +0200

    Add users with name.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,38 @@
 import { io } from 'socket.io-client'
 import World from './world';
 
+let world;
+
 const SERVER = 'http://localhost:3000'
-const socket = io(SERVER)
+const socket = io(SERVER, {
+  autoConnect: false
+})
 
-const world = new World();
+socket.onAny((event, ...args) => {
+  console.log(event, args)
+})
 
-console.log('I am a client', socket)
+socket.on("connect_error", (err) => {
+  if (err.message === "invalid username") {
+    this.usernameAlreadySelected = false;
+  }
+});
+
+
+const connectButton = document.getElementById('connect');
+
+const connect = () => {
+  const input = document.getElementById('player-name')
+  const value = input.value
+  if(!value) {
+    console.error('Fill name!')
+  } else {
+    socket.auth = {
+      playerName: value
+    }
+    socket.connect()
+    world = new World();
+  }
+}
+
+connectButton.onclick  = connect;
--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,25 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
+  const users = []
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      playerName: socket.playerName
+    })
+  }
+  socket.emit("players", users)
+  socket.broadcast.emit(users)
   console.log('User connected');
+})
+
+io.use((socket, next) => {
+  const playerName = socket.handshake.auth.playerName;
+  if(!playerName) {
+    return next(new Error('Invalid playerName', playerName))
+  }
+  socket.playerName = playerName;
+  next()
 })
 
 server.listen(PORT, () => {
